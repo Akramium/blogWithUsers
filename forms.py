5b4f58221e79d7e737c4acab64e20664eb09e844
akramuim <o.akramuim@gmail.com>
2021-06-08 20:18:43 +0100

    update

--- forms.py
+++ forms.py
@@ -28,17 +28,5 @@ class LoginForm(FlaskForm):
 
 # create a comment form
 class CommentForm(FlaskForm):
-    # I will work on it this weekend
-    # TODO: 1 - Create a CommentForm in the form.py file it will only contain a single CKEditorField for users to write their comments.
-    # TODO: Hint: You might need to check the documentation or day 67 to see how we implement the CKEditor.
-    # TODO: 2- Create a Table called Comment where the tablename is "comments". It should contain an id and a text property which will be the primary key and the text entered into the CKEditor.
-    # TODO: 3- Establish a One to Many relationship Between the User Table (Parent) and the Comment table (Child). Where One User is linked to Many Comment objects.
-    # TODO: 4- Establish a One to Many relationship between each BlogPost object (Parent) and Comment object (Child). Where each BlogPost can have many associated Comment objects.
-    # TODO: 5. At this point, with a new Table added, it's a good idea to delete the existing blog.db entirely and to use the line db.create_all() to create all the tables from scratch.
-    # This means you should create a new admin user (id == 1), a new blog post and another user who will make some comments.
-    # TODO: 6. Log in as your John Doe user (or any user that is not the primary user) and make a comment on a blog post. In order for this to work, you will need to update the /post/<int:post_id> route. Make sure that only authenticated (logged-in) users can save their comment. Otherwise, they should see a flash message telling them to log in and redirect them to the /login route.
-    # TODO: 7. Update the code in post.html to display all the comments associated with the blog post.
-    # later in this week
-    # tomorrow i will work on it!
 
     pass
