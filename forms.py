1e9e4b62a8b466a4705a4436086adab696efbcd1
akramuim <o.akramuim@gmail.com>
2021-06-09 21:41:15 +0100

    update

--- forms.py
+++ forms.py
@@ -21,6 +21,7 @@ class RegisterForm(FlaskForm):
 
 
 class LoginForm(FlaskForm):
+
     email = StringField("Email Address", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Let me in!")
